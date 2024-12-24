c64ea4a61410eed883768bd379a250d8059feca8
jmisina <jmisina02@gmail.com>
2024-12-24 11:20:10 +0100

    Refactor for new services

--- src/Products-msvc/ProductsAPI/SQL/NewProductsProcedure.sql
+++ src/Products-msvc/ProductsAPI/SQL/NewProductsProcedure.sql
@@ -1,17 +1,11 @@
-CREATE OR REPLACE PROCEDURE get_new_products()
+CREATE OR REPLACE FUNCTION get_new_products()
+RETURNS TABLE (data JSONB, mt_last_modified TIMESTAMP WITH TIME ZONE)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    PERFORM 1 FROM pg_class WHERE relname = 'temp_new_products';
-    IF FOUND THEN
-        EXECUTE 'DROP TABLE temp_new_products';
-    END IF;
-
-    CREATE TEMP TABLE temp_new_products AS
+    RETURN QUERY
     SELECT data, mt_last_modified
     FROM mt_doc_product
     WHERE mt_last_modified >= NOW() - INTERVAL '7 days';
-
-    RAISE NOTICE 'New products are available in temp_new_products table';
 END;
 $$;
